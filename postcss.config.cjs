b133fa53f578cdf84c2cd54c8ae2d7fd55554134
Odiabackend099 <you@example.com>
2025-08-14 18:52:46 +0100

    build: PS5-safe fixes (no '??'), PostCSS BOM -> .cjs, RL v4, alias lovable-tagger, build ok

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    "plugins":  {
+                    "autoprefixer":  {
+
+                                     },
+                    "tailwindcss":  {
+
+                                    }
+                }
+}
